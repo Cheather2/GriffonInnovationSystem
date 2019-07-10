ae2d466ea9c451f74df4e556219209a67d451529
Corey <corey222222@gmail.com>
2019-07-10 17:21:56 -0500

    edit

--- html/public/index.php
+++ html/public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'../vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
